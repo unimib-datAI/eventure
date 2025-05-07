3ce74aab6bdaaae911391b9a46abda2aca084887
Felix2897 <andrea.feliziani97@gmail.com>
2025-05-07 12:33:38 +0200

    fix auth

--- eventure/components/customheader.tsx
+++ eventure/components/customheader.tsx
@@ -1,28 +1,26 @@
-import React from 'react';
-import { TouchableOpacity, View } from 'react-native';
-import { ChevronLeft } from 'lucide-react-native';
-import { useNavigation } from '@react-navigation/native';
-import ProgressBar from './progressBar';
-import { router } from 'expo-router';
+import React from "react";
+import { TouchableOpacity, View } from "react-native";
+import { ChevronLeft } from "lucide-react-native";
+import { useNavigation } from "@react-navigation/native";
+import ProgressBar from "./progressBar";
+import { router } from "expo-router";
 
 interface CustomHeaderProps {
-    progress: number;
+  progress: number;
 }
 
 const CustomHeader: React.FC<CustomHeaderProps> = ({ progress }) => {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    return (
-        <View className="flex-row  justify-center items-center p-10 bg-white  ">
+  return (
+    <View className="flex-row  justify-center items-center p-10 bg-white  mt-5 ">
+      <TouchableOpacity onPress={() => router.back()} className="right-8">
+        <ChevronLeft size={24} color="#000" />
+      </TouchableOpacity>
 
-            <TouchableOpacity onPress={() => router.back()} className='right-8'   >
-                <ChevronLeft size={24} color="#000" />
-            </TouchableOpacity>
-
-
-            <ProgressBar progress={progress} />
-        </View>
-    );
+      <ProgressBar progress={progress} />
+    </View>
+  );
 };
 
 export default CustomHeader;
